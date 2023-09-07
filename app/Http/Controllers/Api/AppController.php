d953e41c8b810d285a0220f82278761a44a4f327
piouskenny <kehindeu13@gmail.com>
2023-09-07 10:04:48 +0100

    added the functionality to the index method

--- app/Http/Controllers/Api/AppController.php
+++ app/Http/Controllers/Api/AppController.php
@@ -9,7 +9,28 @@ use SmyPhp\Core\Http\Request;
 
 class AppController extends Controller
 {
-    public function index() {
-        
+    public function index(Request $request, Response $response)
+    {
+        if (isset($_GET['slack_name']) && isset($_GET['track'])) {
+            $slackName = $_GET['slack_name'];
+            $track = $_GET['track'];
+
+
+            return $response->json([
+                'slack_name' => $slackName,
+                'current_day' => date('l'), 
+                'utc_time' => gmdate('Y-m-d\TH:i:s\Z'),
+                'track' => $track,
+                'github_file_url' => 'https://github.com/username/repo/blob/main/file_name.ext',
+                'github_repo_url' => 'https://github.com/piouskenny/hngx_api_taskone',
+                'status_code' => 200
+            ], 400);
+
+        } else {
+                return $response->json([
+                    "status" => true,
+                    "message" => "Slack name and track is empty"
+                ], 400);
+        }
     }
 }
